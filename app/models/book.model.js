b214a0a11a4869de5ab36908050c749519f8bfb6
mateus.dev21 <mateus.dev21@gmail.com>
2022-07-27 23:13:06 +0700

    adding book model

---
+++ app/models/book.model.js
@@ -0,0 +1,25 @@
+module.exports = (mongoose) => {
+    const schema = mongoose.Schema(
+        {
+            kode: String,
+            judul: String,
+            penulis: String,
+            penerbit: String,
+            kategori: String,
+            sub_kategori: String,
+            rilis: String,
+            bahasa: String,
+            halaman : String,
+            stok: Int32Array
+        }
+    );
+
+    schema.method('toJSON', function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Book = mongoose.model('books', schema);
+    return Book;
+}
